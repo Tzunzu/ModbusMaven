dbfcdfeb37037fd4f32ee2f9203b95251b986cba
Tzunzu <as@ke.no>
2023-12-21 13:21:44 +0100

    Getting Imgui settup, and start on layout

--- src/main.cpp
+++ src/main.cpp
@@ -9,54 +9,131 @@
 
 #define GLSL_VERSION "#version 330"
 
-int main() {
+int main()
+{
+
+    bool show_demo_window = false;
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(600, 600, "Hello, ImGUI!",
+    GLFWwindow *window = glfwCreateWindow(600, 600, "Modbus Maven",
                                           nullptr, nullptr);
-    if (window == nullptr) {
+    if (window == nullptr)
+    {
         std::cout << "Could not create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         std::cout << "Could not initialize GLAD" << std::endl;
         return -1;
     }
 
     ImGui::CreateContext();
 
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
     io.IniFilename = "imgui.ini";
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(GLSL_VERSION);
 
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window))
+    {
         glfwPollEvents();
 
         glClearColor(0, 0, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Window");
-        {
-            ImGui::Text("Hello, Dear ImGUI!");
-        }
-        ImGui::End();
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
 
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Create a full viewport for the docking space
+        ImGuiViewport *viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->Pos);
+        ImGui::SetNextWindowSize(viewport->Size);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBringToFrontOnFocus;
+        ImGui::Begin("ModbusMavenDock", nullptr, window_flags);
+       
+        ImGui::PopStyleVar(3);
+
+        // Dockspace
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
+
+        // create a menu bar
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Exit"))
+                {
+                    glfwSetWindowShouldClose(window, true);
+                }
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Connect"))
+            {
+                if (ImGui::MenuItem("Conncet to Modbus TCP/IP"))
+                {
+                    // Open a  window to configure the connection
+                    ImGui::OpenPopup("Connect to Modbus TCP/IP");
+                }
+                if (ImGui::MenuItem("Conncet to Modbus RTU"))
+                {
+                    //Open demo window
+                    show_demo_window = true;
+                }
+
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+
+        // Now you can create your window
+        ImGui::Begin("Device List");
+        {
+           ImGui::Button("Add Device", ImVec2(100, 20));
+        }
+        ImGui::End(); // End of Hello, ImGUI! window
+    
+    
+    ImGui::End(); // End of DockSpace
+
+
+    // Rendering
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+// Update Platform and Renderer bindings
+if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+{
+    GLFWwindow* backup_current_context = glfwGetCurrentContext();
+    ImGui::UpdatePlatformWindows();
+    ImGui::RenderPlatformWindowsDefault();
+    glfwMakeContextCurrent(backup_current_context);
+}
+
 
         glfwSwapBuffers(window);
     }
